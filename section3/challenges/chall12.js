2071c04545d72d79be8a4662409a783596e835aa
tmax818 <tylermaxwell661@gmail.com>
2019-11-21 11:54:48 -0800

    update

---
+++ section3/challenges/chall12.js
@@ -0,0 +1,12 @@
+const chalk = require('node_modules')
+
+// playing
+
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+
+console.log(error('Error!'));
+console.log(warning('Warning!'));
+
+const name = 'Sindre';
+console.log(chalk.green('Hello %s'), name);
